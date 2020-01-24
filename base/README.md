655106dbf66c0442e177943c95a36daa6b6cf235
Kurzdor <pavloeksmidt@gmail.com>
2020-01-24 23:15:44 +0200

    initial release

---
+++ base/README.md
@@ -0,0 +1,3 @@
+# cra-template-effector
+
+The Effector template for [Create React App](https://github.com/facebook/create-react-app).
---
+++ base/template/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Counter } from './features/counter'
+import logo from "./logo.svg";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://effector.now.sh/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Effector
+        </a>
+      </header>
+    </div>
+  );
+};
